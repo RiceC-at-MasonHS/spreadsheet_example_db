7f2bc3fbdb0b6da92da88c2fdd802d058ef31b9d
Corey Rice <171101634+RiceC-at-MasonHS@users.noreply.github.com>
2026-04-24 13:08:17 -0400

    only download the spreadsheet once, when container starts

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 
 # Google Sheets CSV export URL - This is allowed for 'public' download
 CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOJSjMWjpTEpNjEcTGwpo_t6gREpp_6tXQhsif1LXgR8tNWVfll7sUYQPS4tNowJLLMhSUzkF093LG/pub?gid=1999930924&single=true&output=csv"
+DOWNLOAD_ONCE = requests.get(CSV_URL)
 
 def get_coffee_data():
-    response = requests.get(CSV_URL)
+    response = DOWNLOAD_ONCE
     response.encoding = 'utf-8'
     # Use DictReader to handle headers automatically
     reader = csv.DictReader(io.StringIO(response.text))
